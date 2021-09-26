37a140cd54d2e7d00afd997a90c1885aac67d8dd
luxianbang <344465120@qq.com>
2021-09-26 15:33:54 +0800

    Log

--- EasyModbus/Log.cs
+++ EasyModbus/Log.cs
@@ -8,6 +8,13 @@ namespace EasyModbus
 {
     class Log
     {
+
+        //获取私有配置文件字符串
+        [System.Runtime.InteropServices.DllImport("kernel32")] private static extern int GetPrivateProfileString(string lpAppName, string lpKeyName, string lpDefault, StringBuilder lpReturnedString, int nSize, string lpFileName);
+
+        //写入私有配置文件字符串
+        [System.Runtime.InteropServices.DllImport("kernel32")] private static extern long WritePrivateProfileString(string mpAppName, string mpKeyName, string mpDefault, string mpFileName);
+
         private Int64 FileSize = 0;//文件大小
 
         public void Write()
@@ -21,5 +28,7 @@ namespace EasyModbus
 
 
         }
+
+
     }
 }
